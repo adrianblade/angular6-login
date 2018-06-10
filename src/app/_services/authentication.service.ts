0e5862ba1e43af0f6f3779a1e43a2f4f7b579f01
Adrian Catalan <acatalan@hiberus.com>
2018-06-10 22:59:15 +0200

    Authentication jwt

---
+++ src/app/_services/authentication.service.ts
@@ -0,0 +1,23 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class AuthenticationService {
+    constructor(private http: HttpClient) { }
+
+    login(username: string, password: string): Observable<string> {
+      const body = `username=${username}&password=${password}`;
+      const options = {
+        headers: new HttpHeaders({ 'Content-Type': 'text/plain' }),
+        responseType: 'text' as 'text'
+      };
+      return this.http.post('http://10.12.0.62:3000/auth/login?username=SSCC&password=SSCC1234', body, options);
+    }
+
+    logout() {
+        // remove user from local storage to log user out
+        localStorage.removeItem('currentUser');
+    }
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,31 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { User } from '../_models';
+import { UserService } from '../_services';
+
+@Component({templateUrl: 'home.component.html'})
+export class HomeComponent implements OnInit {
+    currentUser: string;
+    users: User[] = [];
+
+    constructor(private userService: UserService) {
+        this.currentUser = localStorage.getItem('currentUser');
+    }
+
+    ngOnInit() {
+        this.loadAllUsers();
+    }
+
+    deleteUser(id: number) {
+        this.userService.delete(id).pipe(first()).subscribe(() => {
+            this.loadAllUsers()
+        });
+    }
+
+    private loadAllUsers() {
+        this.userService.getAll().pipe(first()).subscribe(users => {
+            this.users = users;
+        });
+    }
+}
